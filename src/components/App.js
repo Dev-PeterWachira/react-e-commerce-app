364670c258b0d89967ed7b5774a567dbfb6738f7
Dev-PeterWachira <wmainap@gmail.com>
2025-01-30 14:18:55 +0300

    deploying using vercel

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,66 @@
-import React, { useState } from 'react';
-import ProductList from './components/ProductList';
-import Cart from './components/Cart';
+// import React, { useState } from 'react';
+// import ProductList from './components/ProductList';
+// import Cart from './components/Cart';
 
-const sampleProducts = [
-  { id: 1, name: "T-Shirt", price: 20 },
-  { id: 2, name: "Jeans", price: 40 },
-  { id: 3, name: "Sneakers", price: 60 },
-  { id: 4, name: "Hat", price: 15 },
-  { id: 5, name: "Socks", price: 5 },
-];
+// const sampleProducts = [
+//   { id: 1, name: "T-Shirt", price: 20 },
+//   { id: 2, name: "Jeans", price: 40 },
+//   { id: 3, name: "Sneakers", price: 60 },
+//   { id: 4, name: "Hat", price: 15 },
+//   { id: 5, name: "Socks", price: 5 },
+// ];
+
+// function App() {
+//   const [cart, setCart] = useState([]);
+
+//   const addToCart = (product) => {
+//     setCart(prevCart => {
+//       const existingProduct = prevCart.find(item => item.id === product.id);
+//       if (existingProduct) {
+//         return prevCart.map(item =>
+//           item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+//         );
+//       }
+//       return [...prevCart, { ...product, quantity: 1 }];
+//     });
+//   };
+
+//   const removeFromCart = (productId) => {
+//     setCart(prevCart => prevCart.filter(item => item.id !== productId));
+//   };
+
+//   const adjustQuantity = (productId, newQuantity) => {
+//     setCart(prevCart => prevCart.map(item =>
+//       item.id === productId ? { ...item, quantity: newQuantity } : item
+//     ));
+//   };
+
+//   const getTotalPrice = () => {
+//     return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+//   };
+
+//   return (
+//     <div className="App">
+//       <Header />
+//       <ProductList products={sampleProducts} addToCart={addToCart} />
+//       <Cart 
+//         cart={cart} 
+//         removeFromCart={removeFromCart} 
+//         adjustQuantity={adjustQuantity} 
+//         totalPrice={getTotalPrice()} 
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
 
 function App() {
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (product) => {
-    setCart(prevCart => {
-      const existingProduct = prevCart.find(item => item.id === product.id);
-      if (existingProduct) {
-        return prevCart.map(item =>
-          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      }
-      return [...prevCart, { ...product, quantity: 1 }];
-    });
-  };
-
-  const removeFromCart = (productId) => {
-    setCart(prevCart => prevCart.filter(item => item.id !== productId));
-  };
-
-  const adjustQuantity = (productId, newQuantity) => {
-    setCart(prevCart => prevCart.map(item =>
-      item.id === productId ? { ...item, quantity: newQuantity } : item
-    ));
-  };
-
-  const getTotalPrice = () => {
-    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
-  };
-
   return (
-    <div className="App">
-      <Header />
-      <ProductList products={sampleProducts} addToCart={addToCart} />
-      <Cart 
-        cart={cart} 
-        removeFromCart={removeFromCart} 
-        adjustQuantity={adjustQuantity} 
-        totalPrice={getTotalPrice()} 
-      />
+    <div>
+      <h1>Welcome to My E-Commerce App</h1>
     </div>
   );
 }
